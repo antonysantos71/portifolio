fbfc97d41ec29351b4d1967e5d3c07aa79289009
Antony <115037549+antonysantos71@users.noreply.github.com>
2023-04-27 15:52:32 -0300

    Add files via upload

---
+++ portifolio/main.js
@@ -0,0 +1,8 @@
+document.querySelector('.burger').addEventListener ("click" , () => {
+    let itens = document.querySelector("#itens");
+    if (itens.style.display == "block"){
+        itens.style.display = "none"
+    } else {
+        itens.style.display = "block"
+    }
+})
